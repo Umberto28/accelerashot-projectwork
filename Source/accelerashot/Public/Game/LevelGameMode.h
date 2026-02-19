db9f1a88aca0a7100ed0905a9aeab8d671dfdcb4
DiveInTheMetal <davide.carloni01@gmail.com>
2026-02-19 18:57:26 +0100

    Fix LevelGameMode.h edit

--- Source/accelerashot/Public/Game/LevelGameMode.h
+++ Source/accelerashot/Public/Game/LevelGameMode.h
@@ -28,7 +28,7 @@ protected:
 public:
 	/* ----- Properties ----- */
 	UPROPERTY(BlueprintAssignable, Category = "Level Events")
-	static FOnTargetsReset OnTargetsReset;
+	FOnTargetsReset OnTargetsReset;
 	
 	/* ----- Functions ----- */	
 	void OnLevelRestart();
